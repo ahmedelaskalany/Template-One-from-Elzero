c89b39e0bfd7c68dfee81591d44435d8485ca520
AhmedElaskalany <ahmed.elaskalany@hotmail.com>
2022-01-31 20:58:05 +0200

    Updated Glitch for dropdown menu

--- main.js
+++ main.js
@@ -4,14 +4,25 @@ let btn = document.getElementById("button");
 icon.addEventListener("click", toggle);
 
 function toggle() {
-  let ul = document.querySelector("ul");
+  let ul = document.getElementById("dropdown");
 
-  if (ul.style.display !== "block") {
-    ul.style.cssText = "display: block";
-  } else {
-    ul.style.cssText = "display: none";
-  }
+  ul.classList.toggle("active");
+  // if (ul.style.display !== "block") {
+  //   ul.style.cssText = "display: block";
+  //   ul.classList.add("active");
+  // } else {
+  //   ul.style.cssText = "display: none";
+  //   ul.classList.remove("active");
+  // }
 }
+
+window.onclick = function (e) {
+  if (!e.target.matches("#icon") && !e.target.matches("#fnt-icon")) {
+    if (document.getElementById("dropdown").classList.contains("active")) {
+      document.getElementById("dropdown").classList.remove("active");
+    }
+  }
+};
 
 window.onscroll = function () {
   if (window.scrollY > 800) {
